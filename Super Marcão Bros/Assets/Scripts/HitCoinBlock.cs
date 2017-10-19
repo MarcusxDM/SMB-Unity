c6bde2ee830c7e4fb9274de072ee546fe999ae2a
JandersonCRB <jal@ic.ufal.br>
2017-10-19 02:11:16 -0300

    A lot

---
+++ Super Marcão Bros/Assets/Scripts/HitCoinBlock.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitCoinBlock : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D trig)
+    {
+        if(trig.gameObject.tag == "Marcão")
+        {
+            Debug.Log("block");
+        }
+    }
+}
--- Super Marcão Bros/Assets/Scripts/PlayerMovement.cs
+++ Super Marcão Bros/Assets/Scripts/PlayerMovement.cs
@@ -3,30 +3,64 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
-    bool facingRight;
-    public Rigidbody2D player;
+
+    
+    private Rigidbody2D player;
     public Transform   t_player;
+    public Animator    animator;
     [SerializeField] private float MaxSpeed = 10f;                    // The fastest the player can travel in the x axis.
     [SerializeField] private float JumpForce = 400f;                  // Amount of force added when the player jumps.
     [Range(0, 1)] [SerializeField] private float CrouchSpeed = .36f;  // Amount of maxSpeed applied to crouching movement. 1 = 100%
     [SerializeField] private bool AirControl = false;                 // Whether or not a player can steer while jumping;
-    [SerializeField] private LayerMask WhatIsGround;                  // A mask determining what is ground to the character
-                                                                        // Use this for initialization
+    public Transform groundCheck; //Check if the character is on ground
+    [SerializeField] private LayerMask whatIsGround;         // A mask determining what is ground to the character  
+
+    bool facingRight;
+    bool grounded;  // Is in ground
+    float groundRadius = 0.2f;    //How big is gonna be the radius of ground checking point
+    int jumping;
+    bool walking;
+    // Use this for initialization
     void Start () {
+        player = GetComponent<Rigidbody2D>();
         facingRight = true;
+        grounded = false;
 	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
-        if (Input.GetKey("d"))
+
+    private void Update()
+    {
+        walking = player.velocity.x != 0; //Checks if player is walking
+        if (player.velocity.y == 0)
         {
-            if (!facingRight)
-            {
-                Flip();
-            }
-            player.velocity = new Vector2(MaxSpeed, player.velocity.y);
+            jumping = 0;
+        }
+        else if (player.velocity.y > 0)
+        {
+            jumping = 1;
+        }
+        else
+        {
+            jumping = 2;
         }
 
+        animator.SetBool("walking", walking);
+        animator.SetInteger("jumping", jumping);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+        if (grounded)
+        {
+            animator.SetInteger("jumping", 0);
+        }
+        // Check if player is grounded
+        grounded = false;
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheck.position, groundRadius, whatIsGround);
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].gameObject != gameObject)
+                grounded = true;
+        }
         if (Input.GetKey("a"))
         {
             if (facingRight)
@@ -35,12 +69,28 @@ public class PlayerMovement : MonoBehaviour {
             }
             player.velocity = new Vector2(-MaxSpeed, player.velocity.y);
         }
-
+        if (Input.GetKey("d"))
+        {
+            if (!facingRight)
+            {
+                Flip();
+            }
+            player.velocity = new Vector2(MaxSpeed, player.velocity.y);
+        }
         if (Input.GetKey("space"))
         {
-            player.velocity = new Vector2(player.velocity.x, JumpForce);
+            if (grounded)
+            {
+                Jump();
+            }
         }
 
+    }
+
+    void Jump()
+    {
+        animator.SetInteger("jumping", 1);
+        player.velocity = new Vector2(player.velocity.x, JumpForce);
     }
     void Flip()
     {
